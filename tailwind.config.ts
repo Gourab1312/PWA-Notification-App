c53f4fe08991e2a0fbf1d8d2e2b061257ba15739
Gourab Bandyopadhyay <gourabbandyopadhyay.cgr@gmail.com>
2024-04-02 00:33:17 +0530

    matched the ui and the desired functionality

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,26 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontSize: {
+        heading: "29px",
+        "sub-heading": "19px",
+      },
+      colors: {
+        "dim-white": "rgba(255,255,255,0.6)",
+      },
+      margin: {
+        "6.25rem": "6.25rem",
+        "6.75rem": "6.75rem",
+      },
+      padding: {
+        "0.813rem": "0.813rem",
+        "5.719rem": "5.719rem",
+        "2.125rem": "2.125rem",
+        "4.313rem": "4.313rem",
+      },
+      maxHeight: {
+        "21.5rem": "21.5rem",
+      },
     },
   },
   plugins: [],
